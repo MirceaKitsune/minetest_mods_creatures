230548a0f999de590f6de04a8605ef427cd97c7a
MirceaKitsune <sonichedgehog_hyperblast00@yahoo.com>
2014-08-29 02:12:52 +0300

    Localize code responsible for mob navigation

--- creatures/logic_mobs.lua
+++ creatures/logic_mobs.lua
@@ -17,7 +17,7 @@ function logic_mob_step (self, dtime)
 		local player_count = 0
 		for _,obj in ipairs(minetest.env:get_objects_inside_radius(self.object:getpos(), 20)) do
 			if obj:is_player() then
-				player_count = player_count+1
+				player_count = player_count + 1
 			end
 		end
 		if player_count == 0 then
@@ -27,6 +27,8 @@ function logic_mob_step (self, dtime)
 			self.timer_life = 60
 		end
 	end
+
+	local s = self.object:getpos()
 
 	-- physics: make mobs jump when they're stuck
 	if self.jump and self.v_start and self.get_velocity(self) <= 1 and self.object:getvelocity().y == 0 then
@@ -40,26 +42,26 @@ function logic_mob_step (self, dtime)
 		local yaw = self.object:getyaw()
 		local x = math.sin(yaw) * -2
 		local z = math.cos(yaw) * 2
-		self.object:setacceleration({x=x, y=-self.gravity, z=z})
+		self.object:setacceleration({x = x, y= -self.gravity, z = z})
 	else
-		self.object:setacceleration({x=0, y=-self.gravity, z=0})
+		self.object:setacceleration({x = 0, y= -self.gravity, z = 0})
 	end
 
 	-- physics: make mobs tend to stay at the water surface
 	if self.in_liquid then
 		local v = self.object:getvelocity()
-		self.object:setacceleration({x=0, y=self.gravity/(math.max(1, v.y)^2), z=0})
+		self.object:setacceleration({x = 0, y = self.gravity/(math.max(1, v.y) ^ 2), z = 0})
 	end
 
-	-- handle fall damage
+	-- damage: handle fall damage
 	if self.disable_fall_damage and self.object:getvelocity().y == 0 then
 		if not self.old_y then
 			self.old_y = self.object:getpos().y
 		else
 			local d = self.old_y - self.object:getpos().y
 			if d > 5 then
-				local damage = d-5
-				self.object:set_hp(self.object:get_hp()-damage)
+				local damage = d - 5
+				self.object:set_hp(self.object:get_hp() - damage)
 				if self.object:get_hp() == 0 then
 					if self.sounds and self.sounds.die then
 						minetest.sound_play(self.sounds.die, {object = self.object})
@@ -75,22 +77,22 @@ function logic_mob_step (self, dtime)
 		end
 	end
 
-	-- handle environment damage
+	-- damage: handle environment damage
 	self.timer_env_damage = self.timer_env_damage + dtime
 	if self.timer_env_damage > 1 then
 		self.timer_env_damage = 0
-		local s = self.object:getpos()
-		s.y = s.y - 1 -- exclude player offset
-		local n = minetest.env:get_node(s)
+		local pos = s
+		pos.y = pos.y - 1 -- exclude player offset
+		local n = minetest.env:get_node(pos)
 		
 		if self.env_damage.light and self.env_damage.light ~= 0
-			and s.y>0
-			and minetest.env:get_node_light(s)
-			and minetest.env:get_node_light(s) > 4
+			and pos.y > 0
+			and minetest.env:get_node_light(pos)
+			and minetest.env:get_node_light(pos) > 4
 			and minetest.env:get_timeofday() > 0.2
 			and minetest.env:get_timeofday() < 0.8
 		then
-			self.object:set_hp(self.object:get_hp()-self.env_damage.light)
+			self.object:set_hp(self.object:get_hp() - self.env_damage.light)
 			if self.object:get_hp() == 0 then
 				if self.sounds and self.sounds.die then
 					minetest.sound_play(self.sounds.die, {object = self.object})
@@ -106,7 +108,7 @@ function logic_mob_step (self, dtime)
 		if self.env_damage.water and self.env_damage.water ~= 0 and
 			minetest.get_item_group(n.name, "water") ~= 0
 		then
-			self.object:set_hp(self.object:get_hp()-self.env_damage.water)
+			self.object:set_hp(self.object:get_hp() - self.env_damage.water)
 			if self.object:get_hp() == 0 then
 				if self.sounds and self.sounds.die then
 					minetest.sound_play(self.sounds.die, {object = self.object})
@@ -170,8 +172,7 @@ function logic_mob_step (self, dtime)
 	self.timer_think = 0
 
 	-- determine if this mob is in a liquid
-	local pos = self.object:getpos()
-	local node = minetest.env:get_node(pos)
+	local node = minetest.env:get_node(s)
 	local liquidtype = minetest.registered_nodes[node.name].liquidtype
 	if (liquidtype == "source" or liquidtype == "flowing") then
 		self.in_liquid = true
@@ -182,9 +183,8 @@ function logic_mob_step (self, dtime)
 	-- targets: scan for targets to add
 	for _, obj in pairs(minetest.env:get_objects_inside_radius(self.object:getpos(), self.traits.vision)) do
 		if obj ~= self.object and (obj:is_player() or (obj:get_luaentity() and obj:get_luaentity().traits)) and not self.targets[obj] then
-			local s = self.object:getpos()
 			local p = obj:getpos()
-			local dist = ((p.x-s.x)^2 + (p.y-s.y)^2 + (p.z-s.z)^2)^0.5
+			local dist = ((p.x - s.x) ^ 2 + (p.y - s.y) ^ 2 + (p.z - s.z) ^ 2) ^ 0.5
 			if dist < self.traits.vision then
 				local relation = creatures:alliance(self.object, obj)
 				local action = math.random()
@@ -218,7 +218,6 @@ function logic_mob_step (self, dtime)
 
 	-- targets: select a random position to walk to when idle
 	if self.traits.roam >= math.random() then
-		local s = self.object:getpos()
 		local p = {
 			x = math.random(math.floor(s.x - self.traits.vision / 2), math.floor(s.x + self.traits.vision / 2)),
 			y = math.floor(s.y),
@@ -232,9 +231,8 @@ function logic_mob_step (self, dtime)
 		if not target.persist then
 			local target_old = target
 			if target.position or (target.entity:is_player() or target.entity:get_luaentity()) then
-				local s = self.object:getpos()
 				local p = target.position or target.entity:getpos()
-				local dist = ((p.x-s.x)^2 + (p.y-s.y)^2 + (p.z-s.z)^2)^0.5
+				local dist = ((p.x - s.x) ^ 2 + (p.y - s.y) ^ 2 + (p.z - s.z) ^ 2) ^ 0.5
 				local dist_target = target.distance or self.traits.vision
 
 				-- remove targets which are dead or out of range
@@ -256,7 +254,6 @@ function logic_mob_step (self, dtime)
 			end
 
 			if DEBUG_AI_TARGETS then
-				local s = self.object:getpos()
 				local name2 = "creature"
 				if target.entity:is_player() then
 					name2 = target.entity:get_player_name()
@@ -278,9 +275,8 @@ function logic_mob_step (self, dtime)
 	self.target_current = nil
 	local best_priority = 0
 	for i, target in pairs(self.targets) do
-		local s = self.object:getpos()
 		local p = target.position or target.entity:getpos()
-		local dist = ((p.x-s.x)^2 + (p.y-s.y)^2 + (p.z-s.z)^2)^0.5
+		local dist = ((p.x - s.x) ^ 2 + (p.y - s.y) ^ 2 + (p.z - s.z) ^ 2) ^ 0.5
 		local interest = (self.traits.vision * self.traits.determination) / dist
 
 		-- an engine bug occasionally causes incorrect positions, so check that distance isn't 0
@@ -297,29 +293,22 @@ function logic_mob_step (self, dtime)
 		self.v_start = false
 	-- state: attacking, melee
 	elseif self.target_current.objective == "attack" and self.attack_type == "melee" then
-		local s = self.object:getpos()
-		local p = self.target_current.position or self.target_current.entity:getpos()
-		local dist = ((p.x-s.x)^2 + (p.y-s.y)^2 + (p.z-s.z)^2)^0.5
+		self.v_pos = self.target_current.position or self.target_current.entity:getpos()
+		self.v_pos_avoid = false
+		local dist = ((self.v_pos.x - s.x) ^ 2 + (self.v_pos.y - s.y) ^ 2 + (self.v_pos.z - s.z) ^ 2) ^ 0.5
 		local dist_target = self.target_current.distance or self.traits.vision
 
-		local vec = {x=p.x-s.x, y=p.y-s.y, z=p.z-s.z}
-		local yaw = math.atan(vec.z/vec.x)+math.pi/2
-		if p.x > s.x then
-			yaw = yaw+math.pi
-		end
-		self.object:setyaw(yaw)
-
 		if minetest.setting_getbool("fast_mobs") and dist > 2 and dist / dist_target >= 1 - self.target_current.priority then
-			self.set_velocity(self, self.run_velocity)
 			self:set_animation("walk_punch")
+			self.v_speed = self.run_velocity
 			self.v_start = true
 		elseif dist > 2 then
-			self.set_velocity(self, self.walk_velocity)
 			self:set_animation("walk_punch")
+			self.v_speed = self.walk_velocity
 			self.v_start = true
 		else
-			self.set_velocity(self, 0)
 			self:set_animation("punch")
+			self.v_speed = 0
 			self.v_start = false
 			if self.timer_attack > self.traits.attack_interval then
 				self.timer_attack = 0
@@ -328,8 +317,8 @@ function logic_mob_step (self, dtime)
 				end
 				if self.target_current.entity then
 					self.target_current.entity:punch(self.object, 1.0,  {
-						full_punch_interval=1.0,
-						damage_groups = {fleshy=self.attack_damage}
+						full_punch_interval = 1.0,
+						damage_groups = {fleshy = self.attack_damage}
 					}, vec)
 				end
 			end
@@ -340,16 +329,10 @@ function logic_mob_step (self, dtime)
 		end
 	-- state: attacking, shoot
 	elseif self.target_current.objective == "attack" and self.attack_type == "shoot" then
-		local s = self.object:getpos()
-		local p = self.target_current.position or self.target_current.entity:getpos()
-		local vec = {x=p.x-s.x, y=p.y-s.y, z=p.z-s.z}
-		local yaw = math.atan(vec.z/vec.x)+math.pi/2
-		if p.x > s.x then
-			yaw = yaw+math.pi
-		end
-		self.object:setyaw(yaw)
-		self.set_velocity(self, 0)
 		self:set_animation("punch")
+		self.v_pos = self.target_current.position or self.target_current.entity:getpos()
+		self.v_pos_avoid = false
+		self.v_speed = 0
 		self.v_start = false
 
 		if self.timer_attack > self.traits.attack_interval then
@@ -358,14 +341,14 @@ function logic_mob_step (self, dtime)
 				minetest.sound_play(self.sounds.attack, {object = self.object})
 			end
 
-			s.y = s.y + (self.collisionbox[2]+self.collisionbox[5])/2
+			s.y = s.y + (self.collisionbox[2] + self.collisionbox[5]) / 2
 			local obj = minetest.env:add_entity(s, self.attack_arrow)
-			local amount = (vec.x^2+vec.y^2+vec.z^2)^0.5
+			local amount = (vec.x ^ 2 + vec.y ^ 2 + vec.z ^ 2) ^ 0.5
 			local v = obj:get_luaentity().velocity
-			vec.y = vec.y+1
-			vec.x = vec.x*v/amount
-			vec.y = vec.y*v/amount
-			vec.z = vec.z*v/amount
+			vec.y = vec.y + 1
+			vec.x = vec.x * v / amount
+			vec.y = vec.y * v / amount
+			vec.z = vec.z * v / amount
 			obj:setvelocity(vec)
 		end
 
@@ -374,36 +357,24 @@ function logic_mob_step (self, dtime)
 		end
 	-- state: following or avoiding
 	elseif self.target_current.objective == "follow" or self.target_current.objective == "avoid" then
-		local avoid = self.target_current.objective == "avoid"
-
-		local s = self.object:getpos()
-		local p = self.target_current.position or self.target_current.entity:getpos()
-		local dist = ((p.x-s.x)^2 + (p.y-s.y)^2 + (p.z-s.z)^2)^0.5
+		self.v_pos = self.target_current.position or self.target_current.entity:getpos()
+		self.v_pos_avoid = self.target_current.objective == "avoid"
+		local dist = ((self.v_pos.x - s.x) ^ 2 + (self.v_pos.y - s.y) ^ 2 + (self.v_pos.z - s.z) ^ 2) ^ 0.5
 		local dist_target = self.target_current.distance or self.traits.vision
 
-		local vec = {x=p.x-s.x, y=p.y-s.y, z=p.z-s.z}
-		local yaw = math.atan(vec.z/vec.x)+math.pi/2
-		if p.x > s.x then
-			yaw = yaw+math.pi
-		end
-		if avoid then
-			yaw = yaw+math.pi
-		end
-		self.object:setyaw(yaw)
-
 		if minetest.setting_getbool("fast_mobs") and
-		(not avoid and dist / dist_target >= 1 - self.target_current.priority) or
-		(avoid and dist / dist_target < 1 - self.target_current.priority) then
-			self.set_velocity(self, self.run_velocity)
+		(not self.v_pos_avoid and dist / dist_target >= 1 - self.target_current.priority) or
+		(self.v_pos_avoid and dist / dist_target < 1 - self.target_current.priority) then
 			self:set_animation("walk")
+			self.v_speed = self.run_velocity
 			self.v_start = true
 		elseif dist > dist_target / 5 then
-			self.set_velocity(self, self.walk_velocity)
 			self:set_animation("walk")
+			self.v_speed = self.walk_velocity
 			self.v_start = true
 		else
-			self.set_velocity(self, 0)
 			self:set_animation("stand")
+			self.v_speed = 0
 			self.v_start = false
 		end
 
@@ -412,21 +383,35 @@ function logic_mob_step (self, dtime)
 				if self.sounds.random_idle then
 					minetest.sound_play(self.sounds.random_idle, {object = self.object})
 				end
-			elseif avoid and self.sounds.random_avoid then
+			elseif self.v_pos_avoid and self.sounds.random_avoid then
 				minetest.sound_play(self.sounds.random_avoid, {object = self.object})
-			elseif not avoid and self.sounds.random_follow then
+			elseif not self.v_pos_avoid and self.sounds.random_follow then
 				minetest.sound_play(self.sounds.random_follow, {object = self.object})
 			end
 		end
 	end
+
+	-- handle mob nagivation
+	if self.v_pos then
+		local vec = {x = self.v_pos.x - s.x, y = self.v_pos.y - s.y, z = self.v_pos.z - s.z}
+		local yaw = math.atan(vec.z / vec.x) + math.pi / 2
+		if self.v_pos.x > s.x then
+			yaw = yaw + math.pi
+		end
+		if self.v_pos_avoid then
+			yaw = yaw + math.pi
+		end
+		self.object:setyaw(yaw)
+	end
+	self.set_velocity(self, self.v_speed)
 end
 
 -- logic_mob_activate: Executed in on_activate, handles: initialization, static data management
 function logic_mob_activate (self, staticdata, dtime_s)
-	self.object:set_armor_groups({fleshy=self.armor})
-	self.object:setacceleration({x=0, y=-10, z=0})
-	self.object:setvelocity({x=0, y=self.object:getvelocity().y, z=0})
-	self.object:setyaw(math.random(1, 360)/180*math.pi)
+	self.object:set_armor_groups({fleshy = self.armor})
+	self.object:setacceleration({x = 0, y = -10, z = 0})
+	self.object:setvelocity({x = 0, y = self.object:getvelocity().y, z = 0})
+	self.object:setyaw(math.random(1, 360) / 180 * math.pi)
 	if self.attack_type and minetest.setting_getbool("only_peaceful_mobs") then
 		self.object:remove()
 	end
@@ -464,6 +449,7 @@ end
 function logic_mob_punch (self, hitter)
 	local psettings = creatures.player_def[creatures:player_get(hitter)]
 	local relation = creatures:alliance(self.object, hitter)
+	local s = self.object:getpos()
 
 	-- trigger the player's attack sound
 	if hitter:is_player() and psettings.sounds and psettings.sounds.attack then
@@ -504,7 +490,6 @@ function logic_mob_punch (self, hitter)
 			end
 
 			if DEBUG_AI_TARGETS and self.targets[hitter] and (not target_old or self.targets[hitter].objective ~= target_old.objective) then
-				local s = self.object:getpos()
 				local p = hitter:getpos()
 				local name2 = "hitter"
 				if hitter:is_player() then
@@ -523,10 +508,9 @@ function logic_mob_punch (self, hitter)
 		for _, obj in pairs(minetest.env:get_objects_inside_radius(self.object:getpos(), highest_vision)) do
 			if obj ~= self.object and obj:get_luaentity() and obj:get_luaentity().traits then
 				local other = obj:get_luaentity()
-				local s = self.object:getpos()
 				local p = obj:getpos()
 				local h = hitter:getpos()
-				local dist = ((p.x-s.x)^2 + (p.y-s.y)^2 + (p.z-s.z)^2)^0.5
+				local dist = ((p.x - s.x) ^ 2 + (p.y - s.y) ^ 2 + (p.z - s.z) ^ 2) ^ 0.5
 				if dist ~= 0 and dist < other.traits.vision then
 					local relation_other_self = creatures:alliance(obj, self.object)
 					local relation_other_hitter = creatures:alliance(obj, hitter)
